97381f0e8a3c1dc4993d923fdad2893059e8b64b
Roshan Mathurkar <roshanmathurkar007@gmail.com>
2024-01-21 20:45:22 +0530

    episode03

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Using Javascript
 
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Hello world from Javascript.";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
+//React.createelement => object => HTMLElement(Render);
+// const heading = React.createElement(
+//     "h1",
+//     {id:"heading"},
+//     "Episode03 🚀"
+// );
+// console.log(heading);
+//JSX = JavaScript syntax which is easier to create react elements - parcel - babel.
 
-// Using React
-
-// const header = React.createElement("h1",{},"Hello world from react.");
-
-// const div1 = React.createElement("div",{id:"parent"},React.createElement("h1",{},"Parent Heading 1."))
-
-// const div1 = React.createElement("div",
-//     {id:"parent"},[
-//         React.createElement("h1",{},"Parent Heading 1."),
-//         React.createElement("h2",{},"Parent Heading 2.")
-//     ]);
-
-const div1 = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement("div",
-        {id:"children"},
-        [
-            React.createElement("h1",{},"Parent Heading 1."),
-            React.createElement("h2",{},"Parent Heading 2 from react🚀")
-        ]
-    )
+//JSX = converted to React.createElement => React element- JS object => Htmlelement(render).
+const jsxHeading = (
+    <h1 id="jsx-header" className="head">
+        React from JSX 🚀
+    </h1>
 );
+console.log(jsxHeading);
+
+// React functional component
+
+const TitleComp = () => {
+    return <h1>Title from functional component</h1>
+};
+
+const num = 10;
+ /* component composition */
+const HeadingComp2 = () => (
+    <div id="container">
+        <h2>
+        {
+            // we can run any JavaScript code here inside {}.
+            num+20
+        }
+        </h2>
+        <TitleComp/> 
+        <h2>Heading from functional component🚀</h2>
+    </div>
+);
+
+const HeadingComp3 = function(){
+    return (<div id="container">
+            <TitleComp/> 
+            <HeadingComp2/> 
+            <h2>Heading from functional component🚀</h2>
+        </div>)
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div1);
+
+root.render(<HeadingComp2 />);
